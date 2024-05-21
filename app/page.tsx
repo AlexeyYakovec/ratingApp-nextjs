487b15e669865c3eab43a43628062fcf30e5b846
AlexeyYakovec <yakovec8952@gmail.com>
2024-05-21 22:04:24 +0500

    add button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { Htag } from "./components";
+import { Htag, Button } from "./components";
 
 export default function Home() {
    return (
       <main className={styles.main}>
-         <h1>next js</h1>
-         <Htag tag="h3">Htagg</Htag>
+         <Htag tag="h1">next js</Htag>
+         <div className={styles["button-container"]}>
+            <Button appearance="primary">Читать подробнее</Button>
+            <Button appearance="ghost">Читать отзывы</Button>
+         </div>
       </main>
    );
 }
