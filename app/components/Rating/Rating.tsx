625c2b29911e81ecba30c86b27504e7c8853bc04
AlexeyYakovec <yakovec8952@gmail.com>
2024-05-22 13:25:36 +0500

    upgrade rating component

--- app/components/Rating/Rating.tsx
+++ app/components/Rating/Rating.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, KeyboardEvent } from "react";
 import { RatingProps } from "./Rating.props";
 import styles from "./Rating.module.css";
 import cn from "classnames";
@@ -22,7 +22,7 @@ export const Rating = ({
    const constructRating = (currentRating: number) => {
       const updateArray = ratingArray.map((r: JSX.Element, i: number) => {
          return (
-            <StarIcon
+            <span
                className={cn(styles.star, {
                   [styles.filled]: i < currentRating,
                   [styles.editable]: isEditable,
@@ -30,7 +30,14 @@ export const Rating = ({
                onMouseEnter={() => changeDisplay(i + 1)}
                onMouseLeave={() => changeDisplay(rating)}
                onClick={() => onClick(i + 1)}
-            />
+            >
+               <StarIcon
+                  tabIndex={isEditable ? 0 : -1}
+                  onKeyDown={(e: KeyboardEvent<SVGElement>) =>
+                     isEditable && handleSpace(i + 1, e)
+                  }
+               />
+            </span>
          );
       });
       setRatingArray(updateArray);
@@ -50,12 +57,17 @@ export const Rating = ({
       setRating(i);
    };
 
+   const handleSpace = (i: number, e: KeyboardEvent<SVGElement>) => {
+      if (e.code != "Space" || !setRating) {
+         return;
+      }
+      setRating(i);
+   };
+
    return (
       <div {...props} className={styles.rating}>
          {ratingArray.map((rating, i) => (
-            <span className={styles["rating-span"]} key={i}>
-               {rating}
-            </span>
+            <span key={i}>{rating}</span>
          ))}
       </div>
    );
